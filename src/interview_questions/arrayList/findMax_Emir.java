bde6eddc6cd42264efd1fa81fb62550b91321ccd
Emir <emirizg77@gmail.com>
2022-09-28 13:57:26 +0200

    remove e

--- src/interview_questions/arrayList/findMax_Emir.java
+++ src/interview_questions/arrayList/findMax_Emir.java
@@ -1,4 +1,30 @@
 package interview_questions.arrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class findMax_Emir {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,4,44444,5));
+
+        int max = arr.get(0);
+
+        for (Integer eachNum : arr) {
+            if (eachNum>max){
+                max=eachNum;
+            }
+        }
+        System.out.println("max = " + max);
+
+    }
+
 }
+/*
+5. Write a program that can find the maximum number from an ArrayList of integers
+        Ex:
+            list = [1,2,3,4,5];
+            output:
+                5
+ */
