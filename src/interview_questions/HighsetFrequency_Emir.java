e13e2666dd6129bee02ae859d02806efe2bbbf46
Emir <emirizg77@gmail.com>
2022-09-28 13:53:16 +0200

    remove e

---
+++ src/interview_questions/HighsetFrequency_Emir.java
@@ -0,0 +1,23 @@
+package interview_questions;
+
+import java.util.Scanner;
+
+public class HighsetFrequency_Emir {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String str =scan.next();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            int frequency = 0;
+
+
+        }
+
+    }
+
+}
---
+++ src/interview_questions/arrayList/findMax_Emir.java
@@ -0,0 +1,4 @@
+package interview_questions.arrayList;
+
+public class findMax_Emir {
+}
