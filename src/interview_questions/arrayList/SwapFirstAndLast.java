9ae1c321cfc84b32ca0ac1d4ac05efd2d6160529
Merivaonline <melek.soylu@gmail.com>
2022-09-28 14:06:34 +0200

    by melek

---
+++ src/interview_questions/arrayList/SwapFirstAndLast.java
@@ -0,0 +1,32 @@
+package interview_questions.arrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SwapFirstAndLast {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list=new ArrayList<>(Arrays.asList(1,2,3,4,5));
+
+
+        int temp=list.get(0);
+
+        list.set(0,list.get(list.size()-1));
+
+        list.set(list.size()-1,temp);
+
+        System.out.println(list);
+
+    }
+}
+
+
+
+/*
+2. write a program that can swap the first and last elements of an integer arraylist
+                ex:
+                    list = [1,2,3,4,5];
+                    output: [5,2,3,4,1];
+ */
