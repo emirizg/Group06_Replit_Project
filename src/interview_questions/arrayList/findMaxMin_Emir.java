70e6c104445114e5ea59ab96d235701a47d0fd5f
Emir <emirizg77@gmail.com>
2022-09-28 13:59:22 +0200

    remove e

--- src/interview_questions/arrayList/findMaxMin_Emir.java
+++ src/interview_questions/arrayList/findMaxMin_Emir.java
@@ -3,20 +3,25 @@ package interview_questions.arrayList;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class findMax_Emir {
+public class findMaxMin_Emir {
 
     public static void main(String[] args) {
 
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,4,5));
 
         int max = arr.get(0);
+        int min=arr.get(0);
 
         for (Integer eachNum : arr) {
             if (eachNum>max){
                 max=eachNum;
             }
+            if (eachNum<min){
+                min=eachNum;
+            }
         }
         System.out.println("max = " + max);
+        System.out.println("min = " + min);
 
     }
 
