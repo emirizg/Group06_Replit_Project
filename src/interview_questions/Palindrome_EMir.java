1762c1f8bcd3d9ec385e086d1ed83e79785ccc3f
Emir <emirizg77@gmail.com>
2022-09-28 11:42:40 +0200

    remove e

---
+++ src/interview_questions/Palindrome_EMir.java
@@ -0,0 +1,23 @@
+package interview_questions;
+
+public class Palindrome_EMir {
+
+    public static void main(String[] args){
+
+        String str = "Level";
+        String reversed ="";
+
+        for(int i =  str.length()-1; i>=0; i--){
+
+            reversed+=str.charAt(i);
+
+        }
+
+        boolean isPalindrome = str.equalsIgnoreCase(reversed);
+
+        System.out.println("isPalindrome = " + isPalindrome);
+
+    }
+
+
+}
