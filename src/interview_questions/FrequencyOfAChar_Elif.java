44415cce8b27f2547a06afa4931526107ad91f14
Elif <elifzulaldemir98@gmail.com>
2022-09-28 12:38:37 +0200

    frequency

---
+++ src/interview_questions/FrequencyOfAChar_Elif.java
@@ -0,0 +1,46 @@
+package interview_questions;
+
+import java.util.Scanner;
+
+public class FrequencyOfAChar_Elif {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a string and char");
+        String str = scan.next();
+        char ch = scan.next().charAt(0);
+
+        int frequency = 0;
+
+
+        for (int i = 0; i < str.length(); i++) {
+            char each = str.charAt(i);
+            if (each == ch){
+                frequency++;
+            }
+        }
+
+        System.out.println(frequency);
+
+        scan.close();
+
+
+    }
+
+}
+
+/*
+2. Write a program that asks user to enter a string and a char, the returns the frequency of the char from the given string
+            Ex:
+                inputs:
+                    str = "aabcccd";
+                    char = 'c';
+                output: 3
+                inputs:
+                    "Java programming language"
+                    'g'
+                output: 4
+ */
+
